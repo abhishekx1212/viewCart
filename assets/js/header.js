c4688a8576b6f4f412f50c7ab874995c12dfb0de
abhishekx1212 <trigonx1212@gmail.com>
2024-01-30 21:16:22 +0530

    view cart

---
+++ assets/js/header.js
@@ -0,0 +1,34 @@
+const headerData = `
+        <div class="container-fluid">
+            <div class="row bg-dark py-2">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div class="header-logo">
+                        <img src="assets/images/logo.png" alt="" width="130px">
+                    </div>
+                    <div class="header-content">
+                        <nav>
+                            <a href="index.html">Home</a>
+                            <a href="">About</a>
+                            <a href="">Service</a>
+                            <a href="product.html">Products</a>
+                            <a href="cart.html">
+                                <span><i class="fa-solid fa-cart-shopping" style="letter-spacing: -0.8rem"></i></span>
+                                <span id="cartNum" ></span>    
+                            </a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+`;
+
+document.querySelector('#header').innerHTML = headerData;
+const product = JSON.parse(localStorage.getItem('products'));
+function viewData() {
+    if (product != null) {
+        document.getElementById('cartNum').innerHTML = product.length;
+    } else {
+        document.getElementById('cartNum').innerHTML = 0;
+    }
+}
+viewData();
---
+++ assets/js/script1.js
@@ -0,0 +1,54 @@
+const products = [];
+
+var student = [
+    { id: 0, image: 'assets/images/p1.jpg', name: 'APPLE', price: 9000, },
+    { id: 1, image: 'assets/images/p2.jpg', name: 'SAMSUNG', price: 1000, },
+    { id: 2, image: 'assets/images/p3.jpeg', name: 'VIVO', price: 5000, },
+];
+
+for (var i = 0; i < student.length; i++) {
+    document.getElementById('row').innerHTML += `
+    <div class="col-xl-4 col-md-6">
+        <div class="card">
+            <div class="card-content">
+            <img src=${student[i].image} width="100%" alt="">
+            <h2>${student[i].name}</h2>
+            <span class="fs-4 text-success">$${student[i].price}</span>
+            <button onclick=add(${i}) class="btn btn-primary">ADD TO CART</button>
+            </div>
+        </div>
+    </div>
+`;
+}
+
+const add = (productId) => {
+    const cartItem = {
+        id: student[productId].id,
+        name: student[productId].name,
+        price: student[productId].price,
+        image: student[productId].image,
+        quantity: 1
+    };
+    checkQuantity(cartItem);
+}
+
+const checkQuantity = (cartItem) => {
+    const existingItemIndex = products.findIndex((item) => {
+        return item.id === cartItem.id
+    });
+
+    if (existingItemIndex !== -1) {
+        products[existingItemIndex].quantity += 1;
+    } else {
+        products.push(cartItem);
+    }
+
+    saveToLocalStorage();
+    viewData();
+}
+
+
+
+function saveToLocalStorage() {
+    localStorage.setItem('products', JSON.stringify(products));
+}
