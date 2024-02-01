e3adde6732ac97570afced987230b56f4a160dea
abhishekx1212 <trigonx1212@gmail.com>
2024-02-01 14:26:03 +0530

    changes

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,34 @@
 const headerData = `
-        <div class="container-fluid">
-            <div class="row bg-dark py-2">
-                <div class="col-12 d-flex justify-content-between align-items-center">
-                    <div class="header-logo">
-                        <img src="assets/images/logo.png" alt="" width="130px">
-                    </div>
-                    <div class="header-content">
-                        <nav>
-                            <a href="index.html">Home</a>
-                            <a href="">About</a>
-                            <a href="">Service</a>
-                            <a href="product.html">Products</a>
-                            <a href="cart.html">
-                                <span><i class="fa-solid fa-cart-shopping" style="letter-spacing: -0.8rem"></i></span>
-                                <span id="cartNum" ></span>    
-                            </a>
-                        </nav>
-                    </div>
+    <div class="container-fluid">
+        <div class="row bg-dark py-2">
+            <div class="col-12 d-flex justify-content-between align-items-center header-parent">
+                <div class="header-logo">
+                    <img src="assets/images/logo.png" alt="" width="130px">
+                </div>
+                <div class="header-content">
+                    <nav class="nav-1">
+                        <a href="index.html" class="a-hide">Home</a>
+                        <a href="" class="a-hide">About</a>
+                        <a href="" class="a-hide">Service</a>
+                        <a href="product.html" class="a-hide">Products</a>
+                        <a href="cart.html">
+                            <span><i class="fa-solid fa-cart-shopping" style="letter-spacing: -0.8rem"></i></span>
+                            <span id="cartNum" style="letter-spacing: -0.3rem"></span>
+                        </a>
+                        <span><i class="fa-solid fa-bars"></i></span>
+                    </nav>
+                    <nav class="nav-2 text-center bg-dark">
+                        <a href="index.html">Home</a>
+                        <a href="">About</a>
+                        <a href="">Service</a>
+                        <a href="product.html">Products</a>
+                    </nav>
                 </div>
             </div>
         </div>
+    </div>
 `;
+
 
 document.querySelector('#header').innerHTML = headerData;
 const product = JSON.parse(localStorage.getItem('products'));
--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,18 +1,19 @@
 var student = [
-    { id: 0, image: 'assets/images/p1.jpg', name: 'APPLE', price: 9000, },
-    { id: 1, image: 'assets/images/p2.jpg', name: 'SAMSUNG', price: 1000, },
+    { id: 0, image: 'assets/images/p1.png', name: 'APPLE', price: 9000, },
+    { id: 1, image: 'assets/images/p2.jpg', name: 'ONE PLUS', price: 1000, },
     { id: 2, image: 'assets/images/p3.jpeg', name: 'VIVO', price: 5000, },
+    { id: 3, image: 'assets/images/p4.png', name: 'SAMSUNG', price: 7000, },
 ];
 
 for (var i = 0; i < student.length; i++) {
     document.getElementById('row').innerHTML += `
-    <div class="col-xl-4 col-md-6">
-        <div class="card">
+    <div class="col-11 col-lg-3 col-md-6">
+        <div class="card shadow-lg">
             <div class="card-content">
             <img src=${student[i].image} width="100%" alt="">
             <h2>${student[i].name}</h2>
-            <span class="fs-4 text-success">$${student[i].price}</span>
-            <button onclick=add(${i}) class="btn btn-primary">ADD TO CART</button>
+            <span class="fs-4" style="line-height:0.7">$${student[i].price}</span>
+            <button onclick=add(${i}) class="btn btn-primary" style="font-size:20px;">ADD TO CART</button>
             </div>
         </div>
     </div>
